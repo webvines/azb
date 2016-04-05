e9d1c73ea404f5ce2e33d341017e8ca165ec9a57
webvines <dev.webvines@gmail.com>
2016-04-05 17:50:06 +0530

    bug fixes

--- awards/index.php
+++ awards/index.php
@@ -2,166 +2,162 @@
 
 <div class="container">
 	<div class="awards-container">
-		<!-- ROW SECTION 1 -->
-		<div class="row">
-			<div class="awards-date">
-				<h3>2015</h3>
-			</div>
-			<div class="col-md-4">
+		<div class="main-container-awards">
+			<!-- ROW SECTION 1 -->
+			<div class="row">
+				<div class="awards-date">
+					<h3>2015</h3>
+				</div>
+				<div class="col-md-4">
 
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
 
-					<div class="pd-bottom">		
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-				    	<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-				    </div>
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
+						<div class="pd-bottom">		
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+					    	<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+					    </div>
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>
+						<span class="read-awards">Read More +</span>
+					</div>
+
+				</div>
+
+				<div class="col-md-4">
+
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>						
+					</div>
+
+				</div>
+
+
+				<div class="col-md-4">
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>
+						<span class="read-awards">Read More +</span>
+					</div>
+
 				</div>
 
 			</div>
 
-			<div class="col-md-4">
+			<!-- ROW SECTION 2 -->
+			<div class="row awards-pd">
+
+				<div class="col-md-4">
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>						
+					</div>
+
+				</div>
+
+				<div class="col-md-4">
 
 
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>						
+					</div>
+
+				</div>
+
+
+				<div class="col-md-4">
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>						
+					</div>
+
 				</div>
 
 			</div>
 
 
-			<div class="col-md-4">
+			<!-- ROW SECTION 3 -->
+			<div class="row awards-pd">
 
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
+				<div class="col-md-4">
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>						
+					</div>
+
+				</div>
+
+				<div class="col-md-4">
+
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>
+						<span class="read-awards">Read More +</span>
+					</div>
+
+				</div>
+
+
+				<div class="col-md-4">
+
+					<div class="box-awards">
+						<h2>Lorem ipsum dolo</h2>
+						<div class="pd-bottom">	
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+							<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
+						</div>	
+						<!-- IMAGE -->
+						<div class="box-1-img"></div>
+						<span class="read-awards">Read More +</span>
+					</div>
+
 				</div>
 
 			</div>
-
 		</div>
-
-		<!-- ROW SECTION 2 -->
-		<div class="row awards-pd">
-
-			<div class="col-md-4">
-
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
-				</div>
-
-			</div>
-
-			<div class="col-md-4">
-
-
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
-				</div>
-
-			</div>
-
-
-			<div class="col-md-4">
-
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
-				</div>
-
-			</div>
-
-		</div>
-
-
-		<!-- ROW SECTION 3 -->
-		<div class="row awards-pd">
-
-			<div class="col-md-4">
-
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
-				</div>
-
-			</div>
-
-			<div class="col-md-4">
-
-
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
-				</div>
-
-			</div>
-
-
-			<div class="col-md-4">
-
-				<div class="box-awards">
-					<h2>Lorem ipsum dolo</h2>
-					<div class="pd-bottom">	
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-						<p>Lorem ipsum dolor sit amet, sit in fugit essent, timeam adversarium delicatissimi sea te.</p>
-					</div>	
-					<!-- IMAGE -->
-					<div class="box-1-img"></div>
-					<span class="read-awards">Read More +</span>
-				</div>
-
-			</div>
-
-		</div>
-
 	</div>
 
 
